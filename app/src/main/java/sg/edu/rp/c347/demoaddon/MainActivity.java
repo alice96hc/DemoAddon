b51f98218a5ba6e8b2c7c7b99887cc934032feb3
15017199 <15017199@myrp.edu.sg>
2017-07-28 15:47:55 +0800

    finished

--- app/src/main/java/sg/edu/rp/c347/demoaddon/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/demoaddon/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
-//import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Picasso;
+
 
 public class MainActivity extends AppCompatActivity {
     ImageView iv;
@@ -14,10 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        iv = (ImageView) findViewById(R.id.iv);
+        iv = (ImageView) findViewById(R.id.myZoomageView);
 
         //use picasso
-//        String imageUrl = "http://square.github.io/picasso/static/sample.png";
-//        Picasso.with(this).load(imageUrl).into(iv);
+        String imageUrl = "http://square.github.io/picasso/static/sample.png";
+        Picasso.with(this).load(imageUrl).into(iv);
     }
 }
